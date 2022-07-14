5f813e47894e67905be0064748abf7d706ddf820
EoflaOE <eoflaoevicecity@gmail.com>
2022-07-14 10:10:44 +0300

    Remove build and test scripts

--- build.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [string]$p1 = "Release"
-)
-
-dotnet restore
-dotnet build ".\ReadLine.Reboot" -c $p1
-dotnet build ".\ReadLine.Reboot.Demo" -c $p1
-dotnet build ".\ReadLine.Reboot.Tests" -c $p1
--- test.ps1
+++
@@ -1,2 +0,0 @@
-dotnet test .\ReadLine.Reboot.Tests\ReadLine.Reboot.Tests.csproj
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
